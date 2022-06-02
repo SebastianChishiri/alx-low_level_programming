d8cd8b582e3d3d22a528e8f3356473645117db24
Sebastian Chishiri <sebastianchishiri@gmail.com>
2022-06-02 14:39:24 -0700

    2nd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
+ *
  * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ *
+ * Return: 0 if success
+ * .
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
