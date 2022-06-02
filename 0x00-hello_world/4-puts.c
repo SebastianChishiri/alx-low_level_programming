1d295be915ecef9fc725a2af2e8c371f23b878fa
Sebastian Chishiri <sebastianchishiri@gmail.com>
2022-06-02 14:09:31 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+include <stdio.h>
 /**
+ * main - Prints out a string to stdout.
  *
- *main - Prints out a string to stdout.
- *Description: this prints out string
- *Return: 0 if success.
+ * Description: this prins out string
  *
+ * Return: 0 if success.
  */
 int main(void)
 {
