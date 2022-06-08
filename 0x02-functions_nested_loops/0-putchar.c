cbf9b56eed4654834f300552c160498ac38a2a79
Sebastian Chishiri <sebastianchishiri@gmail.com>
2022-06-08 12:49:21 -0700

    try

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,11 @@
  */
 int main(void)
 {
-	int i;
-	putchar ('x)
-	putchar (' ')
-	for (i=1; i<=10; i++)
-	{
-		putchar ('!');
-	}
-	putchar ('\n');
+	_putchar('_');
+
+	_putchar('\n');
 	
-	return 0;
+	return (0)
+	
 }
+
