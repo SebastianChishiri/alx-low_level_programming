6db5d45306a5b8fcfdc606ee9759e5ac901ed577
Sebastian Chishiri <sebastianchishiri@gmail.com>
2022-06-08 16:38:31 -0700

    try

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add -prints add two numbers
+ *
+ * @x: print int x
+ * @y: print int y
+ * Return: return int
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print 10 times the alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int l_a;
+	int count;
+
+	count = 0;
+	while (count <= 9)
+	{
+		for (l_a = 'a'; l_a <= 'z'; l_a++)
+		{
+			_putchar(l_a);
+		}
+		count++;
+		_putchar('\n');
+	}
+}
