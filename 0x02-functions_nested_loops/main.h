3bb204c9889a6213f78c61c7228ccb348da661e5
Sebastian Chishiri <sebastianchishiri@gmail.com>
2022-06-08 15:22:24 -0700

    one

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef _putchar
 #define _putchar
 
-char _putchar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,4 +15,4 @@ int add(int one, int two);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif /* _putchar  */
+#endif /* _putchar */
